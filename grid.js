7ec37bfac7eacc2938dc5497acb74538724eb6d3
Vansh-Gupta-Pep <vansh.gupta@pepcoding.com>
2021-10-19 03:05:05 +0530

    feature of add sheets option

--- grid.js
+++ grid.js
@@ -8,9 +8,12 @@ let underlineBtn = document.querySelector(".underline");
 let italicBtn = document.querySelector(".italic");
 let alignBtns = document.querySelectorAll(".align-container>*");
 let fontSizeElem = document.querySelector(".font-size");
+let formulaBar=document.querySelector(".formula-input");
+
 
 let rows = 100;
 let cols = 26;
+
 
 for (let i = 0; i < rows; i++) {   //create 100 rows
    let colBox = document.createElement("div");  
@@ -41,28 +44,104 @@ for (let i = 0; i < rows; i++) {   // for making grid
 }
 
 
-let sheetDB = [];   //sheet-databse is 2D aaray
-for (let i = 0; i < rows; i++) {
-    let row = [];
-    for (let j = 0; j < cols; j++) {
-        let cell = {    //starting mai initial properties daal diye obj mai
-            bold: "normal",
-            italic: "normal",
-            underline: "none",
-            hAlign: "center",
-            fontFamily: "sans-serif",
-            fontSize: "16",
-            color: "black",
-            bColor: "none",
-            value:"",
-            formula:"",
-            children:[]
-        }
-        row.push(cell);
+
+let btnContainer = document.querySelector(".add-sheet_btn-container");   //sheet add karne wale btn ka selector
+
+let sheetList = document.querySelector(".sheet-list");
+let firstSheet = document.querySelector(".sheet");
+let sheetArray=[]
+let sheetDB;
+
+
+firstSheet.addEventListener("click", makeMeActive)
+firstSheet.click();
+
+btnContainer.addEventListener("click", function () {   //tab chalega jab ham add sheet karenge
+   
+    let AllSheets = document.querySelectorAll(".sheet");   //saari sheets aa gai
+    let lastSheet = AllSheets[AllSheets.length - 1];     //saari sheets mai se last sheet choose kar li
+    let Lastidx = lastSheet.getAttribute("idx");   //last sheet mai se last idx nikal lenge(idx string form mai  aayega)
+    Lastidx = Number(Lastidx);               // String waale last idx ko number mai convert karlia ie "2" ko 2 bana diya
+ 
+    let Newsheet = document.createElement("div");    // create sheet 
+    Newsheet.setAttribute("class", "sheet");
+    Newsheet.setAttribute("idx", `${Lastidx + 1}`);
+    Newsheet.innerText = `Sheet ${Lastidx + 2}`;   //agar sheet ka idx 1 h toh voh sheet no.2 h
+    sheetList.appendChild(Newsheet);                //append
+ 
+    for (let i = 0; i < AllSheets.length; i++) {  //phle saari sheet tab ko unactive karna 
+        AllSheets[i].classList.remove("active");
     }
-    sheetDB.push(row);
+    Newsheet.classList.add("active");      //jo new sheet banai usse active banana h
+    // new sheet create 
+
+    createSheet();
+    sheetDB=sheetArray[Lastidx+1];
+    setUI();
+
+
+    Newsheet.addEventListener("click", makeMeActive)//isse saari newsheets par event listener add ho gya par 1st newsheet nhi h ...therefore humne specifically 1st sheet par event listener laga diya
+ })
+ 
+ 
+ 
+ function makeMeActive(e) {    //jis sheet par click kiya voh active ho jaaye
+
+    let sheet = e.currentTarget;    // jis par evnt listener add hota h uss par currenttarget hota h
+    let AllSheets = document.querySelectorAll(".sheet");
+    for (let i = 0; i < AllSheets.length; i++) {
+        AllSheets[i].classList.remove("active");
+    }
+    sheet.classList.add("active");
+ 
+    let idx=sheet.getAttribute("idx")
+    if(!sheetArray[idx]){
+        createSheet();
+    }
+    sheetDB=sheetArray[idx];
+    setUI();
+ }
+ 
+ 
+
+
+function createSheet(){
+
+    let NewDB = [];   //sheet-databse is 2D aaray
+    for (let i = 0; i < rows; i++) {
+        let row = [];
+        for (let j = 0; j < cols; j++) {
+            let cell = {    //starting mai initial properties daal diye obj mai
+                bold: "normal",
+                italic: "normal",
+                underline: "none",
+                hAlign: "center",
+                fontFamily: "sans-serif",
+                fontSize: "16",
+                color: "black",
+                bColor: "none",
+                value:"",
+                formula:"",
+                children:[]
+            }
+            let elem = document.querySelector(`.grid .cell[rid='${i}'][cid='${j}']`);
+            elem.innerText = "";
+            row.push(cell);
+        }
+        NewDB.push(row);
+    }
+    sheetArray.push(NewDB);
 }
 
+function setUI(){
+    for(let i=0;i<rows;i++){
+        for(let j=0;j<cols;j++){
+            let elem=document.querySelector(`.grid .cell[rid='${i}'][cid='${j}']`);
+            let value=sheetDB[i][j].value;
+            elem.innerText=value;
+        }
+    }
+}
 
 
 let allCells = document.querySelectorAll(".grid .cell");  //".querySelectorAll(".grid .cell")" top par isliye nhi kiya kyuki tab grid cells nhi bane thai...agar tab karte toh error de deta 
@@ -101,7 +180,7 @@ for (let i = 0; i < allCells.length; i++) {
         } else {
             italicBtn.classList.add("active-btn");
         }
-      
+        
         if(cellObject.formula){   //formula h uss cell ke DB mai toh formular bar mai voh formula put kardo
             formulaBar.value=cellObject.formula;
         }else{   //agar DB mai nhi h koi formula uss cell ka toh formmula bar empty kardo
@@ -137,6 +216,7 @@ fontSizeElem.addEventListener("change", function () {   //agar hum font size cha
 boldBtn.addEventListener("click", function () {// Jis bhi cell par click kare voh bold ho jaaye
    let uiCellElement=findUICellElement();
 
+
    let cid = uiCellElement.getAttribute("cid");
     let rid = uiCellElement.getAttribute("rid");
     let cellObject = sheetDB[rid][cid];
@@ -154,6 +234,7 @@ boldBtn.addEventListener("click", function () {// Jis bhi cell par click kare vo
 underlineBtn.addEventListener("click", function () {// Jis bhi cell par click kare voh bold ho jaaye
    let uiCellElement=findUICellElement();
   
+   
    let cid = uiCellElement.getAttribute("cid");
    let rid = uiCellElement.getAttribute("rid");
    let cellObject = sheetDB[rid][cid];
