d56f4c08575436ff7f936f729f26ba5fa3960ebc
Vansh-Gupta-Pep <gvansh.411@gmail.com>
2021-10-21 00:04:12 +0530

    improved NewOpenSave

--- newOpenSave.js
+++ newOpenSave.js
@@ -1,9 +1,31 @@
 let newFile = document.querySelector(".new");
 let openFile = document.querySelector(".open");
-let saveFile = document.querySelector(".download");
+let openModal = document.querySelector(".download");
+let overlay = document.querySelector(".overlay");
+let saveBtn = document.querySelector(".save_file");
+let cancelBtn = document.querySelector(".cancel_file");
+let modal = document.querySelector(".modal");
+let title = document.querySelector("modal>input");
 
-saveFile.addEventListener("click", function () {
+openModal.addEventListener("click", function () {
+  let file_input = document.querySelector(".input");
+
+  modal.classList.remove("modal_remove");
+  overlay.style.display = "block";
   // Download file
+  file_input.addEventListener('change', (e) => {
+		title = e.target.value;
+    console.log(title);
+	});
+
+  cancelBtn.addEventListener("click", function () {
+    modal.classList.add("modal_remove");
+    overlay.style.display = "none";
+  });
+});
+saveBtn.addEventListener("click", function () {
+  console.log(title);
+
   let totalSheets = document.querySelectorAll(".sheet");
   let activeSheet;
   for (let i = 0; i < totalSheets.length; i++) {
@@ -13,16 +35,19 @@ saveFile.addEventListener("click", function () {
       break;
     }
   }
-
   let jsonData = JSON.stringify(sheetDB);
   let file = new Blob([jsonData], { type: "application/json" }); // Convert to file like object
   let fileURL = URL.createObjectURL(file); // Convert to URL for anchor href
 
   let a = document.createElement("a");
   a.href = fileURL;
-  a.download = `Sheet${activeSheet}.json`; // Download attribute with filename
+  console.log("1 time download");
+  a.download = `${title}.json`; // Download attribute with filename
   a.click();
+  modal.classList.add("modal_remove");
+  overlay.style.display = "none";
 });
+
 
 openFile.addEventListener("click", function () {
   // Open file
@@ -56,30 +81,6 @@ openFile.addEventListener("click", function () {
   });
 });
 
-newFile.addEventListener("click", function () {
-  // New file
-  let NewDB = [];   //it is 2D array
-  for (let i = 0; i < rows; i++) {
-      let row = [];
-      for (let j = 0; j < cols; j++) {
-          let cell = {    //starting mai initial properties daal diye obj mai
-              bold: "normal",
-              italic: "normal",
-              underline: "none",
-              hAlign: "center",
-              fontFamily: "sans-serif",
-              fontSize: "16",
-              color: "black",
-              bColor: "none",
-              value:"",
-              formula:"",
-              children:[]
-          }
-          let elem = document.querySelector(`.grid .cell[rid='${i}'][cid='${j}']`);
-          elem.innerText = ""; //UI clear kardia kyuki nayi sheet banai h
-          row.push(cell);
-      }
-      NewDB.push(row);
-  }
-  sheetArray.push(NewDB);//3d array bann gya
+newFile.addEventListener("click", function () {   // New file
+  location.reload();
 });
